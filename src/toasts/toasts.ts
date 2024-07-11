097410dd47de419ef2cca7e02a3337f2166ec21e
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-07-11 10:47:32 -0300

    First Class, making the main page

---
+++ src/toasts/toasts.ts
@@ -0,0 +1,43 @@
+import { Bounce, toast } from "react-toastify";
+
+export const sucessToCopy = () => {
+    toast.success('Copy to clipboard!', {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+    });
+};
+
+export const errorToEnter = (text:string) => {
+    toast.error(text, {
+        position: "bottom-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+    });
+};
+
+export const questionSent = () => {
+    toast.success('The question was sent successfully!', {
+        position: "bottom-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+    });
+}
